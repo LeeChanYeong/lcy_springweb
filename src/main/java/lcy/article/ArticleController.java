a59e9564c64cd634cc860fbf3749655d9d10514c
Lee <lcy>
2019-06-07 09:48:00 +0900

    편지추가

--- src/main/java/lcy/article/ArticleController.java
+++ src/main/java/lcy/article/ArticleController.java
@@ -2,8 +2,6 @@ package lcy.article;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import lcy.book.chap11.Member;
@@ -21,7 +19,7 @@ public class ArticleController {
 	@Autowired
 	ArticleDao articleDao;
 
-	Logger logger = LogManager.getLogger();
+	static final Logger logger = LogManager.getLogger();
 
 	/**
 	 * 글 목록
@@ -52,23 +50,67 @@ public class ArticleController {
 	}
 
 	/**
-	 * 글 등록 화면
-	 */
-	@GetMapping("/article/addForm")
-	public String articleAddForm(HttpSession session) {
-		return "article/addForm";
-	}
-
-	/**
 	 * 글 등록
 	 */
-	@PostMapping("/article/add")
+	@PostMapping("/article/s/add")
 	public String articleAdd(Article article,
 			@SessionAttribute("MEMBER") Member member) {
+		// 세션의 멤버 정보를 글의 등록자 정보에 넣는다.  
 		article.setUserId(member.getMemberId());
 		article.setName(member.getName());
+		
 		articleDao.addArticle(article);
 		return "redirect:/app/article/list";
 	}
-}
 
+	/**
+	 * 글 수정 화면
+	 */
+	@GetMapping("/article/s/edit")
+	public void edit(@RequestParam("articleId") String articleId,
+			@SessionAttribute("MEMBER") Member member, Model model) {
+		Article article = articleDao.getArticle(articleId);
+
+		// 권한 체크 : 세션의 memberId와 글의 userId를 비교
+		if (!member.getMemberId().equals(article.getUserId()))
+			// 자신의 글이 아니면
+			throw new RuntimeException("No Authority!");
+
+		model.addAttribute("article", article);
+	}
+
+	/**
+	 * 글 수정
+	 */
+	@PostMapping("/article/s/update")
+	public String update(Article article,
+			@SessionAttribute("MEMBER") Member member) {
+		article.setUserId(member.getMemberId());
+		int updatedRows = articleDao.updateArticle(article);
+
+		// 권한 체크 : 글이 수정되었는지 확인
+		if (updatedRows == 0)
+			// 글이 수정되지 않음. 자신이 쓴 글이 아님
+			throw new RuntimeException("No Authority!");
+
+		return "redirect:/app/article/view?articleId=" + article.getArticleId();
+	}
+
+	/**
+	 * 글 삭제
+	 */
+	@GetMapping("/article/s/delete")
+	public String delete(@RequestParam("articleId") String articleId,
+			@SessionAttribute("MEMBER") Member member) {
+		int updatedRows = articleDao.deleteArticle(articleId,
+				member.getMemberId());
+
+		// 권한 체크 : 글이 삭제되었는지 확인
+		if (updatedRows == 0)
+			// 글이 삭제되지 않음. 자신이 쓴 글이 아님
+			throw new RuntimeException("No Authority!");
+
+		logger.debug("글을 삭제했습니다. articleId={}", articleId);
+		return "redirect:/app/article/list";
+	}
+}
